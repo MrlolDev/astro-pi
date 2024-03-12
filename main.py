22193776459ff875816e7ace2c2b20105a6d541e
MrlolDev <65485999+MrlolDev@users.noreply.github.com>
2024-03-12 12:07:47 +0100

    Update main.py

--- main.py
+++ main.py
@@ -9,17 +9,17 @@ sense.set_rotation(270)
 # Configurar el sensor de color
 sense.color.gain = 60 # Establecer la sensibilidad del sensor
 sense.color.integration_cycles = 64 # El intervalo en el que se tomará la lectura
-for i in range(28):
-    # Agregar variables de color e imagen
-    m = (255,201,210) # Rosa Oscuro
-    r = (252, 163, 177) # Rosa Claro
-    q = (0, 0, 0) # Negro
-    y = (189,121,132) # Rosa Oscuro
-    t = (111,57,57) # Rosa profundo
-    b = (255,255,255)
+# Agregar variables de color e imagen
+m = (255,201,210) # Rosa Oscuro
+r = (255, 118, 185) # Rosa Claro
+q = (0, 0, 0) # Negro
+y = (189,121,132) # Rosa Oscuro
+t = (111,57,57) # Rosa profundo
+b = (255,255,255)
+for i in range(25):
     imagen = [
-    r,r,r,r,r,r,r,r,
-    r,r,r,r,r,r,r,r,
+    t,t,r,r,r,r,t,t,
+    r,t,r,r,r,r,t,r,
     r,r,r,r,r,r,r,r,
     q,b,r,r,r,r,b,q,
     r,r,m,m,m,m,r,r,
