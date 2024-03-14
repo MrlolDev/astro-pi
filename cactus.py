d13b181a07fcee08a25f00e61b744b9061953e75
MrlolDev <65485999+MrlolDev@users.noreply.github.com>
2024-03-14 17:00:42 +0100

    Create cactus.py

---
+++ cactus.py
@@ -0,0 +1,34 @@
+# Importar las bibliotecas
+from sense_hat import SenseHat
+from time import sleep
+
+# Configurar el Sense HAT
+sense = SenseHat()
+sense.set_rotation(270)
+
+# Configurar el sensor de color
+sense.color.gain = 60 # Establecer la sensibilidad del sensor
+sense.color.integration_cycles = 64 # El intervalo en el que se tomará la lectura
+# Agregar variables de color e imagen
+m = (107,145,19) # Oscuro
+r = (168, 230, 29) # Claro
+n = (0, 0, 0) # Negro
+b = (255,255,255)
+for i in range(28):
+    rgb = sense.color # obtener el color del sensor
+    b = (rgb.red, rgb.green, rgb.blue)
+    imagen = [
+        b,b,b,b,b,b,b,b,
+        b,b,b,r,m,m,b,b,
+        r,b,b,m,m,r,b,b,
+        r,m,b,m,r,m,b,m,
+        m,m,b,r,m,m,b,m,
+        b,r,m,m,r,m,m,r,
+        b,b,m,m,m,m,m,b,
+        b,b,b,m,r,m,b,b,
+    ]
+    
+    # Mostrar la imagen
+    sense.set_pixels(imagen)
+    sleep(1)
+sense.clear()
